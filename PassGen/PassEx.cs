17bc61af3cad9555e045bd599d7c868e15b045f9
stkossman <an.stawski@outlook.com>
2024-06-04 15:42:06 +0300

    Add project files.

---
+++ PassGen/PassEx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassGen
+{
+    public class PassEx
+    {
+        private string capital = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        private string lower = "qwertyuiopasdfghjklzxcvbnm";
+        private string digits = "0123456789";
+        private string special = "!@#$%^&*(){}[]";
+        private string all;
+
+        public PassEx(bool includeCapital, bool includeLower, bool includeDigits, bool includeSpecial)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (includeCapital)
+                sb.Append(capital);
+            if (includeLower)
+                sb.Append(lower);
+            if (includeDigits)
+                sb.Append(digits);
+            if (includeSpecial)
+                sb.Append(special);
+            all = sb.ToString();
+        }
+
+        public int CalculateStrength(string pass)
+        {
+            int score = 0;
+
+            if (pass.Length < 0)
+                score = 0;
+            else if (pass.Length < 10)
+                score += 2;
+            else
+                score += 3;
+
+            if (ContainsLowerCase(pass))
+                score += 1;
+            if (ContainsUpperCase(pass))
+                score += 1;
+            if (ContainsDigit(pass))
+                score += 1;
+            if (ContainsSpecialChar(pass))
+                score += 1;
+
+            return Math.Min(score, 10);
+        }
+
+        private bool ContainsLowerCase(string str)
+        {
+            foreach (char c in str)
+            {
+                if (char.IsLower(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool ContainsUpperCase(string str)
+        {
+            foreach (char c in str)
+            {
+                if (char.IsUpper(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool ContainsDigit(string str)
+        {
+            foreach (char c in str)
+            {
+                if (char.IsDigit(c))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool ContainsSpecialChar(string str)
+        {
+            foreach (char c in str)
+            {
+                if (!char.IsLetterOrDigit(c))
+                    return true;
+            }
+            return false;
+        }
+
+        public string GetLevel(int score)
+        {
+            if (score <= 3)
+                return "simple";
+            else if (score <= 7)
+                return "medium";
+            else
+                return "strong";
+        }
+
+        public string GeneratePassword(int passLen)
+        {
+            StringBuilder sb = new StringBuilder();
+            Random rand = new Random();
+            for (int i = 0; i < passLen; i++)
+            {
+                sb.Append(all[rand.Next(all.Length)]);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ PassGen/Program.cs
@@ -0,0 +1,61 @@
+﻿using PassGen;
+
+Console.WriteLine("1. Include capital letters");
+Console.WriteLine("2. Include lower letters");
+Console.WriteLine("3. Include digits");
+Console.WriteLine("4. Include special characters");
+Console.WriteLine("Enter numbers separated by spaces to choose options (like '1 3 4'):");
+
+string enter =  Console.ReadLine();
+bool includeCapital = false;
+bool includeLower = false;
+bool includeDigits = false;
+bool includeSpecial = false;
+
+string[] choices = enter.Split(' ');
+
+foreach(string choice in choices)
+{
+    int opt;
+    if(int.TryParse(choice, out opt))
+    {
+        switch (opt)
+        {
+            case 1:
+                includeCapital = true;
+                break;
+            case 2:
+                includeLower = true; 
+                break;
+            case 3:
+                includeDigits = true;
+                break;
+            case 4:
+                includeSpecial = true;
+                break;
+            default:
+                Console.WriteLine("Invalid option");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input");
+    }
+}
+
+PassEx passEx = new PassEx(includeCapital, includeLower, includeDigits, includeSpecial);
+
+Console.WriteLine("Enter password length:");
+int passLen;
+if (!int.TryParse(Console.ReadLine(), out passLen))
+{
+    Console.WriteLine("Invalid password length. Using default length (15).");
+    passLen = 15;
+}
+
+string pass = passEx.GeneratePassword(passLen);
+int strength = passEx.CalculateStrength(pass);
+string level = passEx.GetLevel(strength);
+Console.WriteLine($"Suggested password: {pass}");
+Console.WriteLine($"Password's strength = {strength} ({level})");
